6fc9b73ffa35615b130082fe74e5533aad6b5a2e
Joe Jasinski <joe.jasinski@gmail.com>
2024-02-26 23:26:24 -0600

    README and License

--- setup.py
+++ setup.py
@@ -1,9 +1,16 @@
 from setuptools import setup, find_packages
 
+
+from pathlib import Path
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
+
 setup(
     name='ksd',
     version='0.1.0',
     description='Utility for decoding python secretes',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
     author='Joe Jasinski',
     author_email='joe.jasinski@gmail.com',
     url='https://xebia.com/blog/setup-py',
